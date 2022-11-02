4c298b190d368f895e099adad63a408f9950a509
Kowsar7 <kowsar.mojtabaee@gmail.com>
2022-11-02 17:42:05 +0330

    Update parasite

--- src/Pages/QuizPage/Quiz/Parasite/Parasite.js
+++ src/Pages/QuizPage/Quiz/Parasite/Parasite.js
@@ -15,17 +15,32 @@ const parasite = (props) => {
       );
     }
 
-    if (elements[i].id === "titleFade") {
+    // if (elements[i].id === "titleFade") {
+    //   el[i] = (
+    //     <div key="titleFade">
+    //       <ul className={classes.TitleFadeList}>
+    //         <li className={classes.Li1}>{elements[i].inputs.texts[0]}</li>
+    //         <li className={classes.Li2}>{elements[i].inputs.texts[1]}</li>
+    //         <li className={classes.Li3}>{elements[i].inputs.texts[2]}</li>
+    //         <li className={classes.Li4}>{elements[i].inputs.texts[3]}</li>
+    //         <li className={classes.Li5}>{elements[i].inputs.texts[4]}</li>
+    //       </ul>
+    //     </div>
+    //   );
+    // }
+
+    if (elements[i].id === "button") {
       el[i] = (
-        <div key="titleFade">
-          <ul className={classes.TitleFadeList}>
-            <li className={classes.Li1}>{elements[i].inputs.texts[0]}</li>
-            <li className={classes.Li2}>{elements[i].inputs.texts[1]}</li>
-            <li className={classes.Li3}>{elements[i].inputs.texts[2]}</li>
-            <li className={classes.Li4}>{elements[i].inputs.texts[3]}</li>
-            <li className={classes.Li5}>{elements[i].inputs.texts[4]}</li>
-          </ul>
-        </div>
+        <button
+          key="button"
+          className={classes.ParasiteButton}
+          style={{
+            backgroundColor: elements[i].inputs.bgColor,
+            color: elements[i].inputs.textColor,
+          }}
+        >
+          {elements[i].inputs.text}
+        </button>
       );
     }
   }
