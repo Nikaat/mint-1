f4a5a29d679138525043ccad8a08fa695028e049
Kowsar7 <kowsar.mojtabaee@gmail.com>
2022-11-16 14:49:06 +0330

    Update quizIndicator

--- src/Pages/QuizPage/Quiz/Quiz.js
+++ src/Pages/QuizPage/Quiz/Quiz.js
@@ -1,18 +1,16 @@
 import React from "react";
 
 import classes from "./Quiz.module.css";
-// import QuizIndicator from "./quizIndicator/quizIndicator";
+import QuizIndicator from "./quizIndicator/quizIndicator";
 import ProgressContainer from "./progressContainer/progressContainer";
 import QuizContent from "./quizContent/quizContent";
 
 const quiz = (props) => {
   return (
     <div className={classes.QuizContainer}>
-      {/* <div className={classes.Indicator}>
-        <QuizIndicator
-        barFilled={Questions[qNum].barFilled}
-        />
-      </div> */}
+      <div className={classes.Indicator}>
+        <QuizIndicator result={props.result} />
+      </div>
       <ProgressContainer result={props.result} currentProgress />
       <QuizContent result={props.result} />
     </div>
--- src/Pages/QuizPage/Quiz/quizIndicator/quizIndicator.js
+++ src/Pages/QuizPage/Quiz/quizIndicator/quizIndicator.js
@@ -2,15 +2,23 @@ import React from "react";
 
 import classes from "./quizIndicator.module.css";
 
-const quizIndicator = (props) => (
-  <div className={classes.Container}>
-    <div className={classes.Bar}>
-      <div
-        className={classes.BarFilled}
-        style={{ width: props.barFilled }}
-      ></div>
+const quizIndicator = (props) => {
+  const allQuestions = parseInt(props.result.allQuestions, 10);
+  const currentQuestion = parseInt(props.result.currentQuestion, 10);
+
+  const progress = (currentQuestion / allQuestions).toPrecision(3) * 100 + "%";
+
+  console.log(allQuestions, currentQuestion, progress);
+  return (
+    <div className={classes.Container}>
+      <div className={classes.Bar}>
+        <div
+          className={classes.BarFilled}
+          style={{ "--bar-width": progress }}
+        ></div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default quizIndicator;
